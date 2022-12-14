a2bc812cf9b9844cd2b19b09ed9032b8ba920554
Harsh_Shah <96407923+097Harsh@users.noreply.github.com>
2022-12-14 16:42:22 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+			<div class="wthree-copyright">
+			  <p>© SH <a href=""></a></p>
+			</div>
+		  </div>
---
+++ sidebar.php
@@ -0,0 +1,84 @@
+<aside>
+    <div id="sidebar" class="nav-collapse">
+        <!-- sidebar menu start-->
+        <div class="leftside-navigation">
+            <ul class="sidebar-menu" id="nav-accordion">
+                <li>
+                    <a class="active" href="dashboard.php">
+                        <i class="fa fa-dashboard"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-book"></i>
+                        <span>Categroy</span>
+                    </a>
+                    <ul class="sub">
+						<li><a href="add_cat.php">Add Categroy</a></li>
+						<li><a href="all_cat.php">All Categroy</a></li>
+                    </ul>
+                </li>
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-book"></i>
+                        <span>Sub - Categroy</span>
+                    </a>
+                    <ul class="sub">
+						<li><a href="add_subcat.php">Add Sub-Categroy</a></li>
+						<li><a href="all_subcat.php">All Sub-Categroy</a></li>
+                    </ul>
+                </li>
+                <li class="sub-menu">
+                    <a href="javascript:;">
+                        <i class="fa fa-book"></i>
+                        <span>Product</span>
+                    </a>
+                    <ul class="sub">
+						<li><a href="add_pro.php">Add Product</a></li>
+						<li><a href="all_pro.php">All Product</a></li>
+                    </ul>
+                </li>
+                <li class="sub-menu">
+                    <a href="form.php">
+                        <i class="fa fa-tasks"></i>
+                        <span>Form </span>
+                    </a>
+                    
+                </li>
+                <li class="sub-menu">
+                    <a href="table.php">
+                        <i class="fa fa-th"></i>
+                        <span> Tables</span>
+                    </a>
+                    
+                </li>
+               
+                <li class="sub-menu">
+                    <a href="v_user.php">
+                        <i class=" fa fa-bar-chart-o"></i>
+                        <span>View user</span>
+                    </a>
+                   
+                </li>
+                <li class="sub-menu">
+                    <a href="v_feedback.php">
+                        <i class=" fa fa-bar-chart-o"></i>
+                        <span>View Feed-back</span>
+                    </a>
+                    
+                </li>
+                <li class="sub-menu">
+                    <a href="v_inquiry.php">
+                        <i class="fa fa-bar-chart-o"></i>
+                        <span>view inquiry</span>
+                    </a>
+                   
+                </li>
+                
+            </ul>
+        </div>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
