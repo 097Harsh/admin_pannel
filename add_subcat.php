4e54fc9ec505258b23579c6dfb64d2c00704e288
Harsh_Shah <96407923+097Harsh@users.noreply.github.com>
2022-12-14 16:40:58 +0530

    see database and use xampp
    pls see my connection file in common folder name is connect.php

---
+++ add_subcat.php
@@ -0,0 +1,130 @@
+<?php
+
+include 'common/connect.php';
+$cat = $obj->query("select * from category");
+
+if(isset($_POST['submit']))
+{
+    $c_name = $_POST['s_cat'];
+    $s1 	= $_POST['cat'];
+
+    $exe = $obj->query("insert into sub_cat(sub_name,cat_id) values('$c_name','$s1')");
+	if($exe)
+	{
+		echo " <script>alert('Sub Category added succesfully');</script>";
+	}
+	else
+	{
+		echo "<script>alert('Something went wrong...');</script>";
+	}
+}
+
+
+
+
+
+?>
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<head>
+<title>Visitors an Admin Panel Category Bootstrap Responsive Website Template | Form_validation :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstrap-css -->
+<link rel="stylesheet" href="css/bootstrap.min.css" >
+<!-- //bootstrap-css -->
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/style-responsive.css" rel="stylesheet"/>
+<!-- font CSS -->
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="css/font.css" type="text/css"/>
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<script src="js/jquery2.0.3.min.js"></script>
+</head>
+<body>
+<section id="container">
+<!--header start-->
+<?php include 'common/header.php' ?>
+<!--header end-->
+<!--sidebar start-->
+<?php include 'common/sidebar.php' ?>
+<!--sidebar end-->
+<!--main content start-->
+<section id="main-content">
+	<section class="wrapper">
+		<div class="form-w3layouts">
+            <!-- page start-->
+            
+            <div class="row">
+                <div class="col-lg-12">
+                    <section class="panel">
+                        <header class="panel-heading">
+                            Add categroy
+                           
+                        </header>
+                        <div class="panel-body">
+                            <div class="form">
+                                <form class="cmxform form-horizontal " id="signupForm" method="post" action="" novalidate="novalidate">
+                                    <div class="form-group ">
+                                        <label for="s_cat" class="control-label col-lg-3">Sub-Categroy</label>
+                                        <div class="col-lg-6">
+                                            <input class=" form-control" id="s_cat" name="s_cat" type="text">
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <label for="cat" class="control-label col-lg-3">Select categroy</label>
+                                            <div class="col-lg-6">
+                                            <select id="cat" name="cat">
+                                                    <option value="">--select</option>
+                                                    <?php
+                                                            while($s = $cat->fetch_object())
+                                                            {
+                                                                ?>
+                                                                <option value="<?php echo $s->cat_id;?>"><?php echo $s->cat_name;?></option>
+                                                                <?php
+                                                            }
+
+                                                    ?>
+
+                                            </select>
+                                            </div>
+                                    </div>
+                                    <div class="col-lg-offset-3 col-lg-6">
+                                            <button type="submit" class="btn btn-default" id="submit" name="submit">Submit</button> 
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <!-- page end-->
+        </div>
+</section>
+ <!-- footer -->
+		 <?php include 'common/footer.php' ?>
+  <!-- / footer -->
+</section>
+
+<!--main content end-->
+</section>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/jquery.slimscroll.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
+<script src="js/jquery.scrollTo.js"></script>
+</body>
+</html>
---
+++ all_subcat.php
@@ -0,0 +1,109 @@
+<?php
+
+include 'common/connect.php';
+
+$result = $obj->query("select * from sub_cat");
+
+
+?>
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<head>
+<title>Visitors an Admin Panel Category Bootstrap Responsive Website Template | Responsive_table :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Visitors Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- bootstrap-css -->
+<link rel="stylesheet" href="css/bootstrap.min.css" >
+<!-- //bootstrap-css -->
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/style-responsive.css" rel="stylesheet"/>
+<!-- font CSS -->
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<!-- font-awesome icons -->
+<link rel="stylesheet" href="css/font.css" type="text/css"/>
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<script src="js/jquery2.0.3.min.js"></script>
+</head>
+<body>
+<section id="container">
+<!--header start-->
+<?php include 'common/header.php' ?>
+<!--header end-->
+<!--sidebar start-->
+<?php include 'common/sidebar.php' ?>
+<!--sidebar end-->
+<!--main content start-->
+<section id="main-content">
+	<section class="wrapper">
+		<div class="table-agile-info">
+ <div class="panel panel-default">
+    <div class="panel-heading">
+     Sub category table
+    </div>
+    <div>
+      <table class="table" ui-jq="footable" ui-options='{
+        "paging": {
+          "enabled": true
+        },
+        "filtering": {
+          "enabled": true
+        },
+        "sorting": {
+          "enabled": true
+        }}'>
+        <thead>
+          <tr>
+            <th data-breakpoints="xs">ID</th>
+            <th> Sub Category Name</th>
+            <th>Category_id</th>
+            
+          </tr>
+        </thead>
+        <?php
+            while($row = $result->fetch_object())
+            {
+              // echo '<pre>';
+              // print_r($row);
+
+        ?>
+        <tbody>
+          <tr>
+            <td><?php echo $row->sub_id; ?></td>
+            <td><?php echo $row->sub_name; ?></td>
+            <td><?php echo $row->cat_id ?></td>
+          </tr>
+        </tbody>
+        <?php
+           }
+        ?>
+      </table>
+    </div>
+  </div>
+</div>
+</section>
+ <!-- footer -->
+		 <?php include 'common/footer.php' ?>
+  <!-- / footer -->
+</section>
+
+<!--main content end-->
+</section>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/jquery.slimscroll.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
+<script src="js/jquery.scrollTo.js"></script>
+</body>
+</html>
